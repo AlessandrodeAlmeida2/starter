aa6b1c402a2bb57f89515781f7be7342bc828e5b
AlessandrodeAlmeida2 <agostini55@gmail.com>
2024-04-03 13:21:17 -0300

    Filtering

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,9 +1,28 @@
+const { query } = require('express');
 const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
     try {
-        const tours = await Tour.find();
+        // BUILD QUERY
+        const queryObj = {...req.query}
+        const excludedFields = ['page', 'sort', 'limit', 'fields'];
+        excludedFields.forEach(el => delete queryObj[el]);
 
+        // const tours = await Tour.find({
+        //     duration: 5,
+        //     difficulty: 'easy'
+        // });
+
+        const query = Tour.find(queryObj);
+            // .where('duration')
+            // .equals(5)
+            // .where('difficulty')
+            // .equals('easy');
+
+        // EXECUTE QUERY
+        const tours = await query;
+
+        // SEND RESPONSE
         res.status(200).json({
             status: 'success',
             results: tours.length,

4f46769921dbce4777b29e72f6f6a9d206869188
AlessandrodeAlmeida2 <agostini55@gmail.com>
2024-04-04 13:14:46 -0300

    Advanced Filtering

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,6 +4,7 @@ const Tour = require('./../models/tourModel');
 exports.getAllTours = async (req, res) => {
     try {
         // BUILD QUERY
+        // 1) Filtering
         const queryObj = {...req.query}
         const excludedFields = ['page', 'sort', 'limit', 'fields'];
         excludedFields.forEach(el => delete queryObj[el]);
@@ -13,7 +14,12 @@ exports.getAllTours = async (req, res) => {
         //     difficulty: 'easy'
         // });
 
-        const query = Tour.find(queryObj);
+        // 2) Advanded filtering
+        let queryStr = JSON.stringify(queryObj);
+        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+        console.log(JSON.parse(queryStr));
+
+        const query = Tour.find(JSON.parse(queryStr));
             // .where('duration')
             // .equals(5)
             // .where('difficulty')
